0223f6d5c0686ecf254f7b300e28dddedd3ba7d6
Svetka-shv <75161365+Svetka-shv@users.noreply.github.com>
2020-12-10 18:31:06 +0200

    Add files via upload

---
+++ src/components/MovieTabs.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const MovieTabs = (props) => {
+  const { sort_by, updateSortBy } = props;
+  const handleClick = (value) => {
+    return () => {
+      updateSortBy(value);
+    };
+  };
+
+  // componentWillUnmount () {
+
+  // }
+
+  const getClassLink = (value) => {
+    return `nav-link ${sort_by === value ? "active" : ""}`;
+  };
+  return (
+    <ul className="tabs nav nav-pills">
+      <li className="nav-item">
+        <div
+          className={getClassLink("popularity.desc")}
+          onClick={handleClick("popularity.desc")}
+        >
+          Popularity desc
+        </div>
+      </li>
+
+      <li className="nav-item">
+        <div
+          className={getClassLink("revenue.desc")}
+          onClick={handleClick("revenue.desc")}
+        >
+          Revenue desc
+        </div>
+      </li>
+
+      <li className="nav-item">
+        <div
+          className={getClassLink("vote_average.desc")}
+          onClick={handleClick("vote_average.desc")}
+        >
+          Vote average desc
+        </div>
+      </li>
+    </ul>
+  );
+};
+
+export default MovieTabs;
